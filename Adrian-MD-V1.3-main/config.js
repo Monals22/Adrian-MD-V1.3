1cc4e11d7d4523a5caae3fdf56456bd32bce9d4f
Monals22 <136168127+Monals22@users.noreply.github.com>
2023-06-12 16:18:23 +0700

    Add files via upload

---
+++ Adrian-MD-V1.3-main/config.js
@@ -0,0 +1,66 @@
+﻿/*
+    Created & Base By ArxzyDev
+    Jual Sc? Neraka Paling Bawah
+    My Contact https://wa.me/6289513081052
+    
+   Notes:
+   Jika Ingin Recode Silakan Tapi Ingat Creditnya
+   Sc Adrian-MD Akan Terus Di Update.
+   Jika Ingin Beli Apikey Chat Owner Lolhuman
+   
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.apikey = '-' // LOLHUMAN
+global.rosekey = '-' // ROSE
+
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'ON - MAIN BOT?'
+global.namaowner = 'RizzOnly'
+
+//—————「 Setting Owner 」—————//
+global.owner = ['6285843874148']
+global.nomerowner = '62 858-4387-4148'
+global.premium = ['6285843874148', '62858438741480']
+
+//—————「 Set Wm 」—————//
+global.packname = 'STICKER ON MAIN\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.author = 'MALING STIKER?/nNgentod'
+global.prefa = ['', '.']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    success: '🤗Done',
+    admin: '❗Perintah Ini Hanya Bisa Digunakan Oleh Admin Group !',
+    botAdmin: '❗Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: '❗Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: '❗Perintah Ini Hanya Bisa Digunakan Di Group Chat !',
+    private: '❗Perintah Ini Hanya Bisa Digunakan Di Private Chat !',
+    bot: '🤖 Fitur Khusus Pengguna Nomor Bot !',
+    wait: 'Loading Kak Jika Tidak Muncul2 Tandanya Eror!',
+    endLimit: '🕊️ Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12 !',
+    error: '🚫 Fitur Sedang Error !',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 10
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://telegra.ph/file/54bdfdaaeadbda3984b82.jpg'
+global.link = 'https://youtube.com/login'
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
